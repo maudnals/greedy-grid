38e2fb7b95365489bf9aed07e2de29d987d2acbd
D068996 <m.nalpas@sap.com>
2019-03-20 16:15:31 +0100

    added approach

---
+++ main.js
@@ -0,0 +1,12 @@
+/*
+Approach
+--------
+
+1) Look at the monoblock (starting from top left)
+2) For each color: 
+    - Count how many cells of this color are adjacent to this block (directly or not): "colorables"
+    - Take the max of these counts
+3) Color all colorables
+
+Repeat
+*/
